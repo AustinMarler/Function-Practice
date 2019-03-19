ce8c413e843ffbb3f7ff778ed633a67b15a00ef6
Austin Marler <austinnmarler@gmail.com>
2019-03-19 16:32:22 -0700

    finished assignment

--- arrays.js
+++ arrays.js
@@ -19,37 +19,120 @@ var instructors = [
 // 1. Find largest number
 // ---------------------------
 
+findLargestNum(numbers);
+
+function findLargestNum(numArray) {
+    var largestNum = numArray[0];
+    for (var i = 1; i <= numArray.length; i++) {
+        if (largestNum < numArray[i]) {
+            largestNum = numArray[i];
+        }
+    }
+    return largestNum;
+}
 
 // ---------------------------
 // 2. Find longest string
 // ---------------------------
 
+findLongestStr(strings);
+
+function findLongestStr(strArray) {
+    var largestStr = strArray[0];
+    for (var i = 1; i < strArray.length; i++) {
+        if (strArray[i].length > largestStr.length) {
+            largestStr = strArray[i];
+        }
+    }
+    return largestStr;
+}
 
 // ---------------------------
 // 3. Find even numbers
 // ---------------------------
 
+findEvenNums(numbers);
 
+function findEvenNums(numArray) {
+    var evenNumArray = [];
+    for (var i = 0; i <= numArray.length; i++) {
+        if (numArray[i]%2 === 0) {
+            evenNumArray.push(numArray[i]);
+        }
+    }
+    return evenNumArray;
+}
 
 // ---------------------------
 // 4. Find odd numbers
 // ---------------------------
 
+findOddNums(numbers);
+
+function findOddNums(numArray) {
+    var oddNumArray = [];
+    for (var i = 0; i <= numArray.length; i++) {
+        if (numArray[i]%2 === 1) {
+            oddNumArray.push(numArray[i]);
+        }
+    }
+    return oddNumArray;
+}
 
 // ---------------------------
 // 5. Find words that contain `is`
 // ---------------------------
 
+findContainedWord(strings, 'is');
 
+function findContainedWord(strArray, substring) {
+    var containedArray = [];
+    for (var i = 0; i < strArray.length; i++) {
+        if (strArray[i].includes(substring)) {
+            containedArray.push(strArray[i]);
+        }
+    }
+    return containedArray;
+}
 
 // ---------------------------
 // 5. Join Both Arrays Together
 // ---------------------------
 
+joinArrays(numbers, strings);
 
-
+function joinArrays(numArray, strArray) {
+    var joinedArray = [];
+    for (var i = 0; i < numArray.length; i++) {
+        joinedArray.push(numArray[i]);
+    }
+    for (var i = 0; i < strArray.length; i++) {
+        joinedArray.push(strArray[i]);
+    }
+    return joinedArray;
+}
 
 // ---------------------------
 // 6. Use the Instructors array and find all that teach JavaScript, 
 //    then sort them alphabetically
 // ---------------------------
+
+findAndSortObjArray(instructors);
+
+function findAndSortObjArray(instArray) {
+    var newArray = [];
+    for (var i = 0; i < instArray.length; i++) {
+        if (instArray[i].teaches === 'JavaScript') {
+            newArray.push(instArray[i]);
+        }
+    }
+    for (var k = 0; k < newArray.length; k++) {
+        for (var i = 0; i < newArray.length-1; i++) {
+            if (newArray[i].firstname.toLowerCase() > newArray[i+1].firstname.toLowerCase()) {
+                tempValue = newArray[i];
+                newArray[i] = newArray[i+1];
+                newArray[i+1] = tempValue;
+            }
+        }
+    }
+}
--- excercises-2.js
+++ excercises-2.js
@@ -4,6 +4,15 @@
 // if-then-else construct available in JavaScript.
 // ---------------------
 
+function max (a, b) {
+    if (a > b) {
+        return a;
+    }
+    else {
+        return b;
+    }
+}
+
 console.assert(max(2,3) === 3)
 console.assert(max(23,24) === 24)
 console.assert(max(-23, 12) === 12)
@@ -12,6 +21,25 @@ console.assert(max(-23, 12) === 12)
 // Define a function maxOfThree() that takes three 
 // numbers as arguments and returns the largest of them.
 // ---------------------
+
+function maxOfThree (a, b, c) {
+    if (a > b) {
+        if (a > c) {
+            return a;
+        }
+        else {
+            return c;
+        }
+    }
+    else {
+        if (b > c) {
+            return b;
+        }
+        else {
+            return c;
+        }
+    }
+}
 
 console.assert(maxOfThree(2,56,3) === 56)
 console.assert(maxOfThree(12,3,4) === 12)
@@ -22,6 +50,20 @@ console.assert(maxOfThree(-12,4,-5) === 4)
 // string of length 1) and returns true if it is a 
 // vowel, false otherwise.
 // ---------------------
+
+function isVowel (char) {
+    if ((typeof char === 'string') && (isNaN(char))) {
+        if (char.toLowerCase() === 'a' || char.toLowerCase() === 'e' || char.toLowerCase() === 'i' || char.toLowerCase() === 'o' || char.toLowerCase() === 'u') {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+}
 
 console.assert(isVowel(0) === false);
 console.assert(isVowel("B") === false);
@@ -38,6 +80,25 @@ console.assert(isVowel("2") === false)
 // the string "tothohisos isos fofunon".
 // ---------------------
 
+function rovarspraket (str) {
+    var newString = "";
+    if (typeof str === 'string') {
+        for (var i = 0; i < str.length; i++) {
+            var tempChar = str.substr(i, 1);
+            if (!isVowel(tempChar)) {
+                newString = newString + tempChar + 'o' + tempChar;
+            }
+            else {
+                newString = newString + tempChar;
+            }
+        }
+        return newString;
+    }
+    else {
+        return String(str);
+    }
+}
+
 console.assert(rovarspraket("a") === "a")
 console.assert(rovarspraket("b") === "bob")
 console.assert(rovarspraket("cat") === "cocatot")
@@ -51,6 +112,14 @@ console.assert(rovarspraket(0) === "0")
 // return the string "ratset gaj".
 // ---------------------
 
+function reverse (str) {
+    var reverseStr = str.substr(str.length-1, 1);
+    for (var i = str.length-2; i >= 0; i--) {
+        var tempChar = str.substr(i, 1);
+        reverseStr = reverseStr.concat(tempChar);
+    }
+    return reverseStr;
+}
 
 console.assert(reverse("books") === "skoob")
 console.assert(reverse("we don't want no trouble") === "elbuort on tnaw t'nod ew")
